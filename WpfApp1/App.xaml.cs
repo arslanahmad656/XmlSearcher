5e4cf15218d4dc93d67faaf7d1c655d12f8f47fa
Arslan Ahmad <ars.ahm6@gmail.com>
2024-12-04 09:27:08 +0500

    iconification and error logification.

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
+using System.Text;
 using System.Windows;
 
 namespace WpfApp1;
@@ -8,5 +10,51 @@ namespace WpfApp1;
 /// </summary>
 public partial class App : Application
 {
+    private static readonly object _lock = new();
+    private const string fileName = "errors.log";
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                LogError(GetErrorFromException(ex));
+            }
+            else
+            {
+                LogError("Unhandled exception caught by app domain event handler.");
+            }
+        };
+
+        DispatcherUnhandledException += (_, e) => LogError(GetErrorFromException(e.Exception));
+
+        TaskScheduler.UnobservedTaskException += (_, e) => LogError(GetErrorFromException(e.Exception));
+    }
+
+    private static string GetErrorFromException(Exception? ex)
+    {
+        var sb = new StringBuilder();
+        for (int i = 0; ex is not null; i++, ex = ex.InnerException)
+        {
+            if (i > 0)
+            {
+                sb.AppendLine("----------------------------------- INNER EXCEPTION -------------------------------------");
+            }
+
+            sb.AppendLine(ex.GetType().FullName + ": " + ex.Message);
+            sb.AppendLine(ex.StackTrace);
+        }
+
+        return sb.ToString();
+    }
+
+    private static void LogError(string error)
+    {
+        lock (_lock)
+        {
+            File.AppendAllText(fileName, Environment.NewLine + error);
+        }
+    }
 }
 
